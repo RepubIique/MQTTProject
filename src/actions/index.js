0445880105d5b5fd5a0c7d95a4be4dc2434b3600
Ken <ken@cyder.com.sg>
2020-02-21 09:55:38 +0800

    First Axios working example

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import Axios from 'axios';
+
+export const PORT = '8080'
+export const GBP_API_ROOT = `http://localhost:${PORT}`;
+export const GBP_API_KEY = '';
+
+export const fetcher = () => {
+    return Axios.create({
+        baseURL: GBP_API_ROOT,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+    });
+};
--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   IonButton,
   IonCard,
@@ -13,38 +13,69 @@ import {
   IonInput
 } from "@ionic/react";
 
-export const SignUpForm: React.FC = () => (
-  <IonCard>
-    <IonCardHeader>
-      {/*<IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+import { getUsers, login } from '../actions/accounts';
+
+
+export const SignUpForm: React.FC = () => {
+
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [firstname, setFirstname] = useState();
+  const [lastname, setLastname] = useState();
+  const [email, setEmail] = useState();
+
+  // console.log(`Username: ${username} | Password: ${password} | Firstname: ${firstname} | Lastname: ${lastname} | Email: ${email} `);
+
+  const submit = async () => {
+
+    // const data = await getUsers();
+    // if (data) {
+    //   console.log(data)
+    // }
+
+    const data = await login({ username, password });
+    if (data) {
+      console.log(data)
+    }
+
+  }
+
+
+
+  return (
+    <IonCard>
+      <IonCardHeader>
+        {/*<IonCardSubtitle>Card Subtitle</IonCardSubtitle>
       <IonCardTitle>Sign up</IonCardTitle>*/}
-    </IonCardHeader>
-    <IonCardContent>
-      <IonItem>
-        <IonLabel position="stacked">Username</IonLabel>
-        <IonInput></IonInput>
-      </IonItem>
-      <IonItem>
-        <IonLabel position="stacked">Password</IonLabel>
-        <IonInput></IonInput>
-      </IonItem>
-      <IonItem>
-        <IonLabel position="stacked">First Name</IonLabel>
-        <IonInput></IonInput>
-      </IonItem>
-      <IonItem>
-        <IonLabel position="stacked">Last Name</IonLabel>
-        <IonInput></IonInput>
-      </IonItem>
-      <IonItem>
-        <IonLabel position="stacked">Email</IonLabel>
-        <IonInput></IonInput>
-      </IonItem>
-      <section>
-        <IonButton expand="block">Sign Up</IonButton>
-      </section>
-    </IonCardContent>
-  </IonCard>
-);
+      </IonCardHeader>
+      <IonCardContent>
+        <IonItem>
+          <IonLabel position="stacked">Username{<span style={{ color: 'red' }}> * </span>}</IonLabel>
+          <IonInput id={'username'} value={username} onIonChange={(e) => setUsername(e.detail.value)} required></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">Password{<span style={{ color: 'red' }}> * </span>}</IonLabel>
+          <IonInput id={'password'} value={password} onIonChange={e => setPassword(e.detail.value)} required></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">First Name{<span style={{ color: 'red' }}> * </span>}</IonLabel>
+          <IonInput id={'firstname'} value={firstname} onIonChange={e => setFirstname(e.detail.value)} required></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">Last Name{<span style={{ color: 'red' }}> * </span>}</IonLabel>
+          <IonInput id={'lastname'} value={lastname} onIonChange={e => setLastname(e.detail.value)} required></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">Email{<span style={{ color: 'red' }}> * </span>}</IonLabel>
+          <IonInput id={'email'} value={email} onIonChange={e => setEmail(e.detail.value)} required></IonInput>
+        </IonItem>
+        <section>
+          <IonButton expand="block" onClick={submit}>Sign Up</IonButton>
+        </section>
+      </IonCardContent>
+    </IonCard>
+  )
+};
+
 
 export default SignUpForm;
