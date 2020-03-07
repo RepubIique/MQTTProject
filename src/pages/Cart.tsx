5e56d85a832a850245ed095160881c5085cd3995
Ken <kenpoon94@gmail.com>
2020-03-07 20:37:15 +0800

    Fix cart iterator

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -12,52 +12,75 @@ import {
   IonLabel,
   IonInput,
   IonItemDivider,
-  IonCard
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle
 } from "@ionic/react";
 import { GlobalContext } from "../actions/globalContext";
 
 const Cart: React.FC = () => {
   const [state, setState] = useContext(GlobalContext);
-
+  let { cart } = state;
+  let array = [1, 2, 3, 4];
   return (
-    <div>
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot="start">
-              <IonBackButton defaultHref="/tab2" />
-            </IonButtons>
-            <IonTitle>Nanobar</IonTitle>
-          </IonToolbar>
-          <IonItemDivider>
-            <IonLabel>Cart</IonLabel>
-          </IonItemDivider>
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="/tab2" />
+          </IonButtons>
+          <IonTitle>Nanobar</IonTitle>
+        </IonToolbar>
+        <IonItemDivider>
+          <IonLabel>Cart</IonLabel>
+        </IonItemDivider>
 
-          {state.cart.forEach((e: any) => {
-            console.log("this:" + e.name);
-            return <div>{e.name}</div>;
-          })}
+        {cart && cart.length > 0 ? (
+          <IonContent>
+            {cart.map(function(x: any) {
+              return (
+                <IonCard key={x.id}>
+                  <IonCardHeader>
+                    <IonCardSubtitle>{x.name}</IonCardSubtitle>
+                    <IonCardTitle></IonCardTitle>
+                  </IonCardHeader>
+                  <img src={x.image} />
+                  <IonCardContent>
+                    Some Description about the item
+                  </IonCardContent>
+                </IonCard>
+              );
+            })}
+          </IonContent>
+        ) : (
+          <IonContent>
+            <IonCard>
+              <IonCardContent>No Items</IonCardContent>
+            </IonCard>
+          </IonContent>
+        )}
 
-          <IonItemDivider>
-            <IonLabel>Pay With Card</IonLabel>
-          </IonItemDivider>
-          <IonItem>
-            <IonLabel position="stacked">Name</IonLabel>
-            <IonInput placeholder="Name on Card"></IonInput>
-          </IonItem>
-          <IonItem>
-            <IonLabel position="stacked">Email</IonLabel>
-            <IonInput placeholder="send@receipt.com"></IonInput>
-          </IonItem>
-          <IonItem>
-            <IonLabel position="stacked">Credit Card Info</IonLabel>
-            <IonInput placeholder="xxxx-xxxx-xxxx-xxxx"></IonInput>
-            <IonInput placeholder="MM/YY"></IonInput>
-            <IonInput placeholder="CVC"></IonInput>
-          </IonItem>
-        </IonHeader>
-      </IonPage>
-    </div>
+        <IonItemDivider>
+          <IonLabel>Pay With Card</IonLabel>
+        </IonItemDivider>
+        <IonItem>
+          <IonLabel position="stacked">Name</IonLabel>
+          <IonInput placeholder="Name on Card"></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">Email</IonLabel>
+          <IonInput placeholder="send@receipt.com"></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">Credit Card Info</IonLabel>
+          <IonInput placeholder="xxxx-xxxx-xxxx-xxxx"></IonInput>
+          <IonInput placeholder="MM/YY"></IonInput>
+          <IonInput placeholder="CVC"></IonInput>
+        </IonItem>
+      </IonHeader>
+    </IonPage>
   );
 };
 export default Cart;
