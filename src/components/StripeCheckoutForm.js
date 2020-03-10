8ef3f8dfc0da2caa81bebbf961c5e05843271118
Kendrick Bong <Kendrickbong1996@gmail.com>
2020-03-10 23:58:48 +0800

    Dont know why this one is the only one that worked

--- src/components/StripeCheckoutForm.js
+++ src/components/StripeCheckoutForm.js
@@ -1,41 +1,65 @@
 import React from "react";
-import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
-import axios from "axios";
-import "./stripe";
+import { ElementsConsumer, CardElement } from "@stripe/react-stripe-js";
 
 import CardSection from "./StripePayCard";
 
-export default function CheckoutForm() {
-  const stripe = useStripe();
-  const elements = useElements();
-
-  const handleSubmit = async event => {
+class CheckoutForm extends React.Component {
+  handleSubmit = async event => {
+    // We don't want to let default form submission happen here,
+    // which would refresh the page.
     event.preventDefault();
 
-    const { error, paymentMethod } = await stripe.createPaymentMethod({
-      type: "card",
-      card: elements.getElement(CardElement)
-    });
-    console.log(paymentMethod);
-    // if (!error) {
-    //   const { id } = paymentMethod;
+    const { stripe, elements } = this.props;
 
-    //   try {
-    //     const { data } = await axios.post("/api/checkout", {
-    //       id,
-    //       amount: 1099
-    //     });
-    //     console.log(data);
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
-    // }
+    if (!stripe || !elements) {
+      // Stripe.js has not yet loaded.
+      // Make  sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    const result = await stripe.confirmCardPayment(
+      "pi_1GLAS8C6jkVKarGSLUQkEU8f_secret_BziKBTKHgPFMFVZ4K9SSZ0XlL",
+      {
+        payment_method: {
+          card: elements.getElement(CardElement),
+          billing_details: {
+            name: "Jenny Rosen"
+          }
+        }
+      }
+    );
+
+    if (result.error) {
+      // Show error to your customer (e.g., insufficient funds)
+      console.log(result.error.message);
+    } else {
+      // The payment has been processed!
+      if (result.paymentIntent.status === "succeeded") {
+        // Show a success message to your customer
+        // There's a risk of the customer closing the window before callback
+        // execution. Set up a webhook or plugin to listen for the
+        // payment_intent.succeeded event that handles any business critical
+        // post-payment actions.
+      }
+    }
   };
 
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <CardSection />
+        <button disabled={!this.props.stripe}>Confirm order</button>
+      </form>
+    );
+  }
+}
+
+export default function InjectedCheckoutForm() {
   return (
-    <form onSubmit={handleSubmit}>
-      <CardSection />
-      <button disabled={!stripe}>Confirm order</button>
-    </form>
+    <ElementsConsumer>
+      {({ stripe, elements }) => (
+        <CheckoutForm stripe={stripe} elements={elements} />
+      )}
+    </ElementsConsumer>
   );
 }
--- src/components/stripe.js
+++ src/components/stripe.js
@@ -1,26 +1,38 @@
+// const stripe = require("stripe")("sk_test_cupKvj0K2Ty0I3ZVar0Vk5PN00ioJfhbWc");
+
+// export default async (req, res) => {
+//   const { id, amount } = req.body;
+
+//   try {
+//     const payment = await stripe.paymentIntents.create({
+//       amount,
+//       currency: "aud",
+//       description: "pass in later",
+//       payment_method: id,
+//       confirm: true
+//     });
+
+//     console.log(payment);
+
+//     return res.status(200).json({
+//       confirm: "It is sent"
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(400).json({
+//       message: "Not happening"
+//     });
+//   }
+// };
+
 const stripe = require("stripe")("sk_test_cupKvj0K2Ty0I3ZVar0Vk5PN00ioJfhbWc");
 
-export default async (req, res) => {
-  const { id, amount } = req.body;
-
-  try {
-    const payment = await stripe.paymentIntents.create({
-      amount,
-      currency: "aud",
-      description: "pass in later",
-      payment_method: id,
-      confirm: true
-    });
-
-    console.log(payment);
-
-    return res.status(200).json({
-      confirm: "It is sent"
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({
-      message: "Not happening"
-    });
-  }
-};
+(async () => {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1099,
+    currency: "aud",
+    // Verify your integration in this guide by including this parameter
+    metadata: { integration_check: "accept_a_payment" }
+  });
+  console.log(paymentIntent);
+})();
