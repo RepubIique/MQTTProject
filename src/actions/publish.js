69ee4484d40724c59b65707e55bd577083b7a41b
Ken <ken@cyder.com.sg>
2020-03-13 18:12:29 +0800

    The setup

--- src/actions/publish.js
+++ src/actions/publish.js
@@ -1,48 +1,44 @@
-const MQTTConnect = () => {
-  function collection() {
-    let mqtt = require("mqtt");
-    let topic = "vendingmachine2/command";
-    const options = {
-      port: 16987,
-      host: "mqtt://hairdresser.cloudmqtt.com",
-      clientId: "KendrickTest",
-      username: "gdyqmxhb",
-      password: "qYlscJNX0e5F",
-      keepalive: 60,
-      reconnectPeriod: 1000,
-      protocolId: "MQIsdp",
-      protocolVersion: 3,
-      clean: true,
-      encoding: "utf8"
-    };
-    const client = mqtt.connect("mqtt://hairdresser.cloudmqtt.com", options);
-    client.on("connect", function() {
-      // When connected
-      console.log("connected");
+let mqtt = require('mqtt');
 
-      client.subscribe("vendingmachine2/feedback", error => {
-        if (error) console.error(error);
-        else {
-          client.publish(topic, "0");
-          openDoor();
-        }
-      });
-    });
+export const MQTTConnect = () => {
+	let topic = 'vendingmachine2/command';
+	const options = {
+		port: 16987,
+		host: 'mqtt://hairdresser.cloudmqtt.com',
+		clientId: 'KendrickTest',
+		username: 'gdyqmxhb',
+		password: 'qYlscJNX0e5F',
+		keepalive: 60,
+		reconnectPeriod: 1000,
+		protocolId: 'MQIsdp',
+		protocolVersion: 3,
+		clean: true,
+		encoding: 'utf8',
+		timeout: 3,
+		useSSL: true
+	};
+	const client = mqtt.connect('mqtt://hairdresser.cloudmqtt.com', options);
+	client.on('connect', function() {
+		// When connected
+		console.log('connected');
 
-    client.on("message", (topic, message) => {
-      console.log(topic, message.toString());
-    });
+		client.subscribe('vendingmachine2/feedback', (error) => {
+			if (error) console.error(error);
+			else {
+				client.publish(topic, '0');
+				openDoor();
+			}
+		});
+	});
+	client.on('message', (topic, message) => {
+		console.log(topic, message.toString());
+	});
 
-    let door = [1, 2, 3, 4];
+	let door = [ 1, 2, 3, 4 ];
 
-    function openDoor() {
-      for (let i = 0; i < door.length; i++) {
-        client.publish(topic, `${door[i]}`);
-      }
-    }
-  }
-
-  return { collection };
+	function openDoor() {
+		for (let i = 0; i < door.length; i++) {
+			client.publish(topic, `${door[i]}`);
+		}
+	}
 };
-
-export default MQTTConnect;
