1c7feeb6df922f9f612efb0d87b108940b0d4565
Ken <ken@cyder.com.sg>
2020-03-14 09:41:05 +0800

    Opening correct doors

--- src/components/StripeCheckoutForm.js
+++ src/components/StripeCheckoutForm.js
@@ -1,67 +1,64 @@
-import React, { useContext, useState } from "react";
-import mqtt from "mqtt";
-import { IonButton, IonCard, IonCardContent } from "@ionic/react";
-import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
-import { stripePaymentMethodHandler, storeOrder } from "../actions/api/payment";
-import CardSection from "./StripePayCard";
-import { GlobalContext } from "../actions/globalContext";
-import GlobalActions from "../actions/globalActions";
-import CartActions from "../actions/cartActions";
-import { MQTTConnect } from "../actions/publish";
+import React, { useContext, useState } from 'react';
+import mqtt from 'mqtt';
+import { IonButton, IonCard, IonCardContent } from '@ionic/react';
+import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
+import { stripePaymentMethodHandler, storeOrder } from '../actions/api/payment';
+import CardSection from './StripePayCard';
+import { GlobalContext } from '../actions/globalContext';
+import GlobalActions from '../actions/globalActions';
+import CartActions from '../actions/cartActions';
+import { MQTTConnect } from '../actions/publish';
 
 export default function CheckoutForm() {
-  const {
-    toggleShowPaymentLoading,
-    toggleShowPaymentAlert,
-    changePaymentStatus
-  } = GlobalActions();
-  const { clearCart, checkOut } = CartActions();
-  const [state, setState] = useContext(GlobalContext);
-  const { currentuser } = state;
+	const { toggleShowPaymentLoading, toggleShowPaymentAlert, changePaymentStatus } = GlobalActions();
+	const { clearCart, checkOut } = CartActions();
+	const [ state, setState ] = useContext(GlobalContext);
+	const { currentuser } = state;
 
-  const stripe = useStripe();
-  const elements = useElements();
+	const stripe = useStripe();
+	const elements = useElements();
 
-  const handleSubmit = async event => {
-    event.preventDefault();
-    //toggleShowPaymentLoading();
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		//toggleShowPaymentLoading();
 
-    if (!stripe || !elements) {
-      // Stripe.js has not yet loaded.
-      // Make sure to disable form submission until Stripe.js has loaded.
-      return;
-    }
+		if (!stripe || !elements) {
+			// Stripe.js has not yet loaded.
+			// Make sure to disable form submission until Stripe.js has loaded.
+			return;
+		}
 
-    const result = await stripe.createPaymentMethod({
-      type: "card",
-      card: elements.getElement(CardElement),
-      billing_details: {
-        // Include any additional collected billing details.
-        name: currentuser.first_name + " " + currentuser.last_name
-      }
-    });
+		const result = await stripe.createPaymentMethod({
+			type: 'card',
+			card: elements.getElement(CardElement),
+			billing_details: {
+				// Include any additional collected billing details.
+				name: currentuser.first_name + ' ' + currentuser.last_name
+			}
+		});
 
-    stripePaymentMethodHandler(result).then(res => {
-      if (res.error === null) {
-        storeOrder(checkOut()).then(() => {
-          clearCart();
-          MQTTConnect();
-          toggleShowPaymentAlert(true);
-        });
-      } else {
-        toggleShowPaymentAlert(false);
-      }
-    });
-  };
+		stripePaymentMethodHandler(result).then((res) => {
+			let obj = checkOut();
+			if (res.error === null) {
+				storeOrder(obj).then(() => {
+					clearCart();
+					MQTTConnect(obj.cart);
+					toggleShowPaymentAlert(true);
+				});
+			} else {
+				toggleShowPaymentAlert(false);
+			}
+		});
+	};
 
-  return (
-    <IonCard>
-      <IonCardContent>
-        <CardSection />
-        <IonButton expand="block" onClick={handleSubmit}>
-          Pay
-        </IonButton>
-      </IonCardContent>
-    </IonCard>
-  );
+	return (
+		<IonCard>
+			<IonCardContent>
+				<CardSection />
+				<IonButton expand='block' onClick={handleSubmit}>
+					Pay
+				</IonButton>
+			</IonCardContent>
+		</IonCard>
+	);
 }
