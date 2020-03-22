f647b021b0ce0e35794fbf0e3e66f819f57d1d2c
Kendrick Bong <Kendrickbong1996@gmail.com>
2020-03-22 21:13:47 +0800

    initial heroku commit

--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
-import Axios from 'axios';
+import Axios from "axios";
 
-export const PORT = '8080'
+export const PORT = process.env.PORT || 8080;
 export const GBP_API_ROOT = `http://localhost:${PORT}`;
-export const GBP_API_KEY = '';
+export const GBP_API_KEY = "";
 
 export const fetcher = () => {
-    return Axios.create({
-        baseURL: GBP_API_ROOT,
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        },
-    });
+  return Axios.create({
+    baseURL: GBP_API_ROOT,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    }
+  });
 };
