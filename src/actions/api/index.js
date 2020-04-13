c68279ca0cd0457f48d6d7f102371f8f08e0527b
Republique <kendrickbong1996@gmail.com>
2020-04-13 19:41:31 +0800

    http to https

--- src/actions/api/index.js
+++ src/actions/api/index.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 export const PORT = process.env.PORT || 8080;
-export const GBP_API_ROOT = `http://104.248.152.188`;
+export const GBP_API_ROOT = `https://104.248.152.188`;
 export const GBP_API_KEY = "";
 
 export const fetcher = () => {
